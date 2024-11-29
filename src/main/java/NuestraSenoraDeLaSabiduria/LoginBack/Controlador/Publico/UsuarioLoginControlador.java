d717240042afeb349a9132e3d4436f89fc1b9740
Diego <58009401+marzo245@users.noreply.github.com>
2024-11-28 22:41:09 -0500

    feat: update repository migration

---
+++ src/main/java/NuestraSenoraDeLaSabiduria/LoginBack/Controlador/Publico/UsuarioLoginControlador.java
@@ -0,0 +1,41 @@
+package NuestraSenoraDeLaSabiduria.LoginBack.Controlador.Publico;
+
+import NuestraSenoraDeLaSabiduria.LoginBack.Controlador.AuthResponse;
+import NuestraSenoraDeLaSabiduria.LoginBack.Servicio.UsuarioServicio;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Controlador para la entidad Usuario
+ *
+ * @version 1.1
+ * @Autor Diego Chicuazuque
+ */
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioLoginControlador {
+
+  @Autowired
+  private UsuarioServicio usuarioServicio;
+
+  public UsuarioLoginControlador(UsuarioServicio usuarioServicio) {
+    this.usuarioServicio = usuarioServicio;
+  }
+
+  // Con estos cambios, el metodo login ahora utiliza
+  // @PostMapping, lo cual es más apropiado para recibir
+  // datos en el cuerpo de la solicitud.
+  @PostMapping("/login")
+  public ResponseEntity<AuthResponse> login(
+    @RequestBody Map<String, String> requestBody
+  ) throws Exception {
+    String nombreUsuario = requestBody.get("nombreUsuario");
+    String contrasena = requestBody.get("contrasena");
+
+    return ResponseEntity.ok(
+      usuarioServicio.loginUsuario(nombreUsuario, contrasena)
+    );
+  }
+}
