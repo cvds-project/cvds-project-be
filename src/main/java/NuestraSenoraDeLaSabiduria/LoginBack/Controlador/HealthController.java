16367217f355f9274f7943e5596c3624e793208f
JFM1028 <116533238+JFM1028@users.noreply.github.com>
2024-11-28 21:56:38 -0500

    feat: pruebita namas

---
+++ src/main/java/NuestraSenoraDeLaSabiduria/LoginBack/Controlador/HealthController.java
@@ -0,0 +1,13 @@
+package NuestraSenoraDeLaSabiduria.LoginBack.Controlador;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String health() {
+        return "hola mundoooo" +
+                "!!!";
+    }
+}
