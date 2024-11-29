927726bd779e8e79c2394e05b5650093cdb4071f
Diego <58009401+marzo245@users.noreply.github.com>
2024-11-28 23:05:37 -0500

    Feature/repository migration (#3)
    * feat: update repository migration
    
    * feat: Corrigiendo migracion

--- src/main/java/NuestraSenoraDeLaSabiduria/LoginBack/Configuracion/CorsConfig.java
+++ src/main/java/NuestraSenoraDeLaSabiduria/LoginBack/Configuracion/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
       public void addCorsMappings(CorsRegistry registry) {
         registry
           .addMapping("/**")
-          .allowedOrigins("http://localhost:3000")
+          .allowedOriginPatterns("*") 
           .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
           .allowedHeaders("*")
           .allowCredentials(true);
@@ -23,3 +23,4 @@ public class CorsConfig {
     };
   }
 }
+
