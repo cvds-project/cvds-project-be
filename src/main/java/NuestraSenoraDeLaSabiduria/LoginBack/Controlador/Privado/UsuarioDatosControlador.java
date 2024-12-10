46e3f8384329ffa06ff323b38a37af74659d61c7
Diego <58009401+marzo245@users.noreply.github.com>
2024-12-10 14:09:26 -0500

    feat: Nuevo appi(endpoint)
    a

--- src/main/java/NuestraSenoraDeLaSabiduria/LoginBack/Controlador/Privado/UsuarioDatosControlador.java
+++ src/main/java/NuestraSenoraDeLaSabiduria/LoginBack/Controlador/Privado/UsuarioDatosControlador.java
@@ -1,12 +1,20 @@
 package NuestraSenoraDeLaSabiduria.LoginBack.Controlador.Privado;
 
+import NuestraSenoraDeLaSabiduria.LoginBack.Servicio.UsuarioServicio;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/usuario")
 @RequiredArgsConstructor
 public class UsuarioDatosControlador {
-    
+
+  final UsuarioServicio usuarioServicio;
+
+  @RequestMapping("/datosUsuario")
+  public String datosUsuario(@RequestParam String usuarioId) {
+    return usuarioServicio.obtenerNombreEstudiante(usuarioId);
+  }
 }
